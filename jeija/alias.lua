dbd9b4e6b75bd996ae8529516861407bb942bfe4
Anthony Zhang <azhang9@gmail.com>
2012-01-30 13:10:03 -0500

    Add aliases for plugs, sockets, inverters, and buttons.

--- jeija/alias.lua
+++ jeija/alias.lua
@@ -7,9 +7,15 @@ minetest.register_alias("jeija:wireless_inverter", "jeija:wireless_inverter_on")
 minetest.register_alias("jeija:wireless_receiver", "jeija:wireless_receiver_off")
 minetest.register_alias("jeija:wireless_transmitter", "jeija:wireless_transmitter_off")
 minetest.register_alias("jeija:switch", "jeija:mesecon_switch_off")
+minetest.register_alias("jeija:wall_button", "jeija:wall_button_off")
 minetest.register_alias("jeija:piston", "jeija:piston_normal")
 minetest.register_alias("jeija:blinky_plant", "jeija:blinky_plant_off")
 minetest.register_alias("jeija:mesecon_torch", "jeija:mesecon_torch_on")
 minetest.register_alias("jeija:hydro_turbine", "jeija:hydro_turbine_off")
 minetest.register_alias("jeija:pressure_plate_stone", "jeija:pressure_plate_stone_off")
 minetest.register_alias("jeija:pressure_plate_wood", "jeija:pressure_plate_wood_off")
+
+if ENABLE_TEMPEREST==1 then
+	minetest.register_alias("jeija:mesecon_socket", "jeija:mesecon_socket_off")
+	minetest.register_alias("jeija:mesecon_inverter", "jeija:mesecon_inverter_off")
+end
