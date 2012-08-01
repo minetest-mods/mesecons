35de798dcd78809ae50629e3bcf46e9d93b1729e
Kotolegokot <gkotolegokot@gmail.com>
2012-08-01 16:48:00 +0600

    Blinky Plant

--- mesecons_blinkyplant/init.lua
+++ mesecons_blinkyplant/init.lua
@@ -1,30 +1,38 @@
 -- The BLINKY_PLANT
 
 minetest.register_node("mesecons_blinkyplant:blinky_plant_off", {
-	drawtype = "plantlike",
+	drawtype = "torchlike",
 	visual_scale = 1,
 	tile_images = {"jeija_blinky_plant_off.png"},
 	inventory_image = "jeija_blinky_plant_off.png",
 	paramtype = "light",
 	walkable = false,
-	groups = {snappy=2},
+	groups = {dig_immediate=3},
     	description="Blinky Plant",
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.1, -0.5, -0.1, 0.1, -0.5+0.6, 0.1},
+	},
 })
 
 minetest.register_node("mesecons_blinkyplant:blinky_plant_on", {
-	drawtype = "plantlike",
+	drawtype = "torchlike",
 	visual_scale = 1,
 	tile_images = {"jeija_blinky_plant_on.png"},
 	inventory_image = "jeija_blinky_plant_off.png",
 	paramtype = "light",
 	walkable = false,
-	groups = {snappy=2, not_in_creative_inventory=1},
+	groups = {dig_immediate=3, not_in_creative_inventory=1},
 	drop='"mesecons_blinkyplant:blinky_plant_off" 1',
 	light_source = LIGHT_MAX-7,
 	description = "Blinky Plant",
 	after_dig_node = function(pos)
 		mesecon:receptor_off(pos)
-	end
+	end,
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.1, -0.5, -0.1, 0.1, -0.5+0.6, 0.1},
+	},
 })
 
 minetest.register_craft({
