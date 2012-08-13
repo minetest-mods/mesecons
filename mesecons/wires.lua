68342c79d54b0efb4d9a680c17fbe2bb33be440a
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2012-08-12 22:31:35 -0400

    Made X <-> Z bends not produce junction bump (only appears on T-junctions, crossings, etc)

--- mesecons/wires.lua
+++ mesecons/wires.lua
@@ -87,7 +87,8 @@ for zmy=0, 1 do
 	if zpy == 1 then table.insert(nodebox, box_zpy) end
 	if xmy == 1 then table.insert(nodebox, box_xmy) end
 	if zmy == 1 then table.insert(nodebox, box_zmy) end
-	if adjx and adjz then
+	nobump = xp+zp+xm+zm
+	if adjx and adjz and (nobump > 2) then
 		table.insert(nodebox, box_bump1)
 		table.insert(nodebox, box_bump2)
 		tiles_off = {
