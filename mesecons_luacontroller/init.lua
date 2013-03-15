e1211729ccea38a2ef5a77c544ae122ff40db14a
Anthony Zhang <azhang9@gmail.com>
2013-03-15 17:07:15 -0400

    Avoid borking the luacontroller if the code uses square brackets. When the next stable MT is released, uncomment the formspec_escape call to take advantave of formspec escaping.

--- mesecons_luacontroller/init.lua
+++ mesecons_luacontroller/init.lua
@@ -284,9 +284,13 @@ end
 local reset_meta = function(pos, code, errmsg)
 	local meta = minetest.env:get_meta(pos)
 	code = code or "";
+	code = string.gsub(code, "%[", "(") -- would otherwise
+	code = string.gsub(code, "%]", ")") -- corrupt formspec
 	errmsg = errmsg or "";
 	errmsg = string.gsub(errmsg, "%[", "(") -- would otherwise
 	errmsg = string.gsub(errmsg, "%]", ")") -- corrupt formspec
+	--code = minetest.formspec_escape(code or "")
+	--errmsg = minetest.formspec_escape(errmsg or "")
 	meta:set_string("code", code)
 	meta:set_string("formspec", "size[10,8]"..
 		"background[-0.2,-0.25;10.4,8.75;jeija_luac_background.png]"..
