ab5820f215751c56da478acac57f0a80d839c34b
Megaf <mmegaf@gmail.com>
2014-11-22 23:18:03 +0000

    Separeted Fences to it's own mod. Changed name to Mese Fence.

--- mesecons_random/init.lua
+++ mesecons_random/init.lua
@@ -23,61 +23,6 @@ minetest.register_craft({
 	}
 })
 
--- CONDUCTING FENCE
-minetest.register_node("mesecons_random:conductingfence", {
-	description="A fence that conducts electricity",
-	is_ground_content = true,
-	drawtype = "fencelike",
-	tiles = {"electricfence_tile.png"},
-	inventory_image = "electricfence.png",
-	wield_image = "electricfence.png",
-	paramtype = "light",
-	selection_box = {
-	        type = "fixed",
-	        fixed = {-1/7, -1/2, -1/7, 1/7, 1/2, 1/7},
-	},
-	groups = {snappy=3},
-	sounds = default.node_sound_wood_defaults(),
-	walkable = true,
-	mesecons = {conductor = {
-		state = mesecon.state.off,
-		rules = { --axes
-			{x = -1, y = 0, z = 0},
-			{x = 1, y = 0, z = 0},
-			{x = 0, y = -1, z = 0},
-			{x = 0, y = 1, z = 0},
-			{x = 0, y = 0, z = -1},
-			{x = 0, y = 0, z = 1},
-		},
-		onstate = "mesecons_random:conductingfence_active"
-	}}
-})
-
-minetest.register_node("mesecons_random:conductingfence_active", {
-	is_ground_content = true,
-	drawtype = "fencelike",
-	tiles = {"electricfence_tile.png^electricfence_tile_active.png"},
-	paramtype = "light",
-	selection_box = {
-	        type = "fixed",
-	        fixed = {-1/7, -1/2, -1/7, 1/7, 1/2, 1/7},
-	},
-	groups = {snappy=3},
-	sounds = default.node_sound_wood_defaults(),
-	walkable = true,
-	mesecons = {conductor = {
-		state = mesecon.state.on,
-		rules = { --axes
-			{x = -1, y = 0, z = 0},
-			{x = 1, y = 0, z = 0},
-			{x = 0, y = -1, z = 0},
-			{x = 0, y = 1, z = 0},
-			{x = 0, y = 0, z = -1},
-			{x = 0, y = 0, z = 1},
-		},
-		offstate = "mesecons_random:conductingfence"
-	}}
-})
 
 minetest.register_craft({
 	output = 'mesecons_random:conductingfence 4',
@@ -96,7 +41,7 @@ minetest.register_node("mesecons_random:ghoststone", {
 	is_ground_content = true,
 	inventory_image = minetest.inventorycube("jeija_ghoststone_inv.png"),
 	groups = {cracky=3},
-	sounds = default.node_sound_stone_defaults(),
+	--sounds = default.node_sound_stone_defaults(),
 	mesecons = {conductor = {
 		state = mesecon.state.off,
 		rules = { --axes
