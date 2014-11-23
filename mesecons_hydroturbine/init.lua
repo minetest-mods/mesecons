775d2501125cfcb489f615b8ec2cd855ca68e4ce
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2014-11-23 18:05:01 -0500

    Add meshes for water turbine and and wall lever instead of nodeboxes Adjust textures and inventory images to it

--- mesecons_hydroturbine/init.lua
+++ mesecons_hydroturbine/init.lua
@@ -4,24 +4,17 @@
 -- (does not work with other liquids)
 
 minetest.register_node("mesecons_hydroturbine:hydro_turbine_off", {
-	drawtype = "nodebox",
+	drawtype = "mesh",
+	mesh = "jeija_hydro_turbine.obj",
 	tiles = {"jeija_hydro_turbine_off.png"},
+	inventory_image = "jeija_hydro_turbine_inv.png",
+	wield_scale = {x=0.75, y=0.75, z=0.75},
 	groups = {dig_immediate=2},
     	description="Water Turbine",
 	paramtype = "light",
 	selection_box = {
 		type = "fixed",
-		fixed = {{-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
-			{-0.15, 0.5, -0.15, 0.15, 1.45, 0.15},
-			{-0.45, 1.15, -0.1, 0.45, 1.45, 0.1},
-			{-0.1, 1.15, -0.45, 0.1, 1.45, 0.45}},
-	},
-	node_box = {
-		type = "fixed",
-		fixed = {{-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
-			{-0.15, 0.5, -0.15, 0.15, 1.45, 0.15},
-			{-0.45, 1.15, -0.1, 0.45, 1.45, 0.1},
-			{-0.1, 1.15, -0.45, 0.1, 1.45, 0.45}},
+		fixed = { -0.5, -0.5, -0.5, 0.5, 1.5, 0.5 },
 	},
 	sounds = default.node_sound_stone_defaults(),
 	mesecons = {receptor = {
@@ -30,25 +23,18 @@ minetest.register_node("mesecons_hydroturbine:hydro_turbine_off", {
 })
 
 minetest.register_node("mesecons_hydroturbine:hydro_turbine_on", {
-	drawtype = "nodebox",
+	drawtype = "mesh",
+	mesh = "jeija_hydro_turbine.obj",
+	wield_scale = {x=0.75, y=0.75, z=0.75},
 	tiles = {"jeija_hydro_turbine_on.png"},
+	inventory_image = "jeija_hydro_turbine_inv.png",
 	drop = "mesecons_hydroturbine:hydro_turbine_off 1",
 	groups = {dig_immediate=2,not_in_creative_inventory=1},
 	description="Water Turbine",
 	paramtype = "light",
 	selection_box = {
 		type = "fixed",
-		fixed = {{-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
-			{-0.15, 0.5, -0.15, 0.15, 1.45, 0.15},
-			{-0.5, 1.15, -0.1, 0.5, 1.45, 0.1},
-			{-0.1, 1.15, -0.5, 0.1, 1.45, 0.5}},
-	},
-	node_box = {
-		type = "fixed",
-		fixed = {{-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
-			{-0.15, 0.5, -0.15, 0.15, 1.45, 0.15},
-			{-0.5, 1.15, -0.1, 0.5, 1.45, 0.1},
-			{-0.1, 1.15, -0.5, 0.1, 1.45, 0.5}},
+		fixed = { -0.5, -0.5, -0.5, 0.5, 1.5, 0.5 },
 	},
 	sounds = default.node_sound_stone_defaults(),
 	mesecons = {receptor = {
--- mesecons_walllever/init.lua
+++ mesecons_walllever/init.lua
@@ -3,9 +3,9 @@
 -- Powers the block 2 nodes behind (using a receiver)
 mesecon.register_node("mesecons_walllever:wall_lever", {
 	description="Lever",
-	drawtype = "nodebox",
-	inventory_image = "jeija_wall_lever_off.png",
-	wield_image = "jeija_wall_lever_off.png",
+	drawtype = "mesh",
+	inventory_image = "jeija_wall_lever_inv.png",
+	wield_image = "jeija_wall_lever_inv.png",
 	paramtype = "light",
 	paramtype2 = "facedir",
 	sunlight_propagates = true,
@@ -24,40 +24,16 @@ mesecon.register_node("mesecons_walllever:wall_lever", {
 		minetest.sound_play("mesecons_lever", {pos=pos})
 	end
 },{
-	tiles = { "jeija_wall_lever_tb.png", "jeija_wall_lever_bottom.png",
-		"jeija_wall_lever_sides.png", "jeija_wall_lever_sides.png",
-		"jeija_wall_lever_back.png", "jeija_wall_lever_off.png",
-	},
-	node_box = {
-		type = "fixed",
-		fixed = {{ -6/16, -6/16, 6/16, 6/16,  6/16, 8/16 },	-- the base "slab"
-			 { -5/16, -3/16, 5/16, 5/16,  3/16, 6/16 },	-- the lighted ring area
-			 { -4/16, -2/16, 4/16, 4/16,  2/16, 5/16 },	-- the raised bit
-			 { -2/16, -1/16, 3/16, 2/16,  1/16, 4/16 },	-- the lever "hinge"
-			 { -1/16, -8/16, 4/16, 1/16,  0,    6/16 }}	-- the lever itself.
-	},
+	tiles = { "jeija_wall_lever_off.png" },
+	mesh="jeija_wall_lever_off.obj",
 	mesecons = {receptor = {
 		rules = mesecon.rules.buttonlike_get,
 		state = mesecon.state.off
 	}},
 	groups = {dig_immediate = 2, mesecon_needs_receiver = 1}
 },{
-	tiles = {
-		"jeija_wall_lever_top.png",
-		"jeija_wall_lever_tb.png",
-		"jeija_wall_lever_sides.png",
-		"jeija_wall_lever_sides.png",
-		"jeija_wall_lever_back.png",
-		"jeija_wall_lever_on.png",
-	},
-	node_box = {
-		type = "fixed",
-		fixed = {{ -6/16, -6/16,  6/16, 6/16, 6/16,  8/16 },	-- the base "slab"
-			 { -5/16, -3/16,  5/16, 5/16, 3/16,  6/16 },	-- the lighted ring area
-			 { -4/16, -2/16,  4/16, 4/16, 2/16,  5/16 },	-- the raised bit
-			 { -2/16, -1/16,  3/16, 2/16, 1/16,  4/16 },	-- the lever "hinge"
-			 { -1/16,  0,     4/16, 1/16, 8/16,  6/16 }}	-- the lever itself.
-	},
+	tiles = { "jeija_wall_lever_on.png" },
+	mesh="jeija_wall_lever_on.obj",
 	mesecons = {receptor = {
 		rules = mesecon.rules.buttonlike_get,
 		state = mesecon.state.on
