d0354d0dab68d5927cfcccfddab67613d35cef79
Jeija <norrepli@gmail.com>
2015-07-27 11:51:02 +0200

    Fix #234

--- mesecons/settings.lua
+++ mesecons/settings.lua
@@ -1,7 +1,12 @@
 -- SETTINGS
 function mesecon.setting(setting, default)
-	if type(default) == "bool" then
-		return minetest.setting_getbool("mesecon."..setting) or default
+	if type(default) == "boolean" then
+		local read = minetest.setting_getbool("mesecon."..setting)
+		if read == nil then
+			return default
+		else
+			return read
+		end
 	elseif type(default) == "string" then
 		return minetest.setting_get("mesecon."..setting) or default
 	elseif type(default) == "number" then
