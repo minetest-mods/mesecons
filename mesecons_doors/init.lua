f1a42835f24dd8a50e6fbe0a5f0752a202fe5b85
HybridDog <ovvv@web.de>
2016-02-27 11:43:09 +0100

    toggle doors instead of opening and closing them, fixes solar panel flowers

--- mesecons_doors/init.lua
+++ mesecons_doors/init.lua
@@ -52,13 +52,13 @@ local function meseconify_door(name)
 				action_on = function(pos, node)
 					local door = doors.get(pos)
 					if door then
-						door:open()
+						door:toggle()
 					end
 				end,
 				action_off = function(pos, node)
 					local door = doors.get(pos)
 					if door then
-						door:close()
+						door:toggle()
 					end
 				end,
 				rules = mesecon.rules.pplate
@@ -95,13 +95,13 @@ if doors and doors.get then
 			action_on = function(pos, node)
 				local door = doors.get(pos)
 				if door then
-					door:open()
+					door:toggle()
 				end
 			end,
 			action_off = function(pos, node)
 				local door = doors.get(pos)
 				if door then
-					door:close()
+					door:toggle()
 				end
 			end,
 		}},
