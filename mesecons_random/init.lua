03ab151c873fd6a4574a8490b17a48b4e9e65d76
Wuzzy <almikes@aol.com>
2017-02-15 23:00:02 +0100

    Fix active ghoststones being droppable with sand
    Fixes #289

--- mesecons_random/init.lua
+++ mesecons_random/init.lua
@@ -53,6 +53,7 @@ minetest.register_node("mesecons_random:ghoststone_active", {
 	diggable = false,
 	sunlight_propagates = true,
 	paramtype = "light",
+	drop = "mesecons_random:ghoststone",
 	mesecons = {conductor = {
 		state = mesecon.state.on,
 		rules = {
