c538ac15e726c9bcd83ae80caffea5d27db26b03
DS-Minetest <vorunbekannt75@web.de>
2017-04-11 15:42:05 +0200

    make formspec bigger

--- mesecons_luacontroller/init.lua
+++ mesecons_luacontroller/init.lua
@@ -479,12 +479,12 @@ local function reset_meta(pos, code, errmsg)
 	meta:set_string("code", code)
 	code = minetest.formspec_escape(code or "")
 	errmsg = minetest.formspec_escape(errmsg or "")
-	meta:set_string("formspec", "size[10,8]"..
-		"background[-0.2,-0.25;10.4,8.75;jeija_luac_background.png]"..
-		"textarea[0.2,0.6;10.2,5;code;;"..code.."]"..
-		"image_button[3.75,6;2.5,1;jeija_luac_runbutton.png;program;]"..
-		"image_button_exit[9.72,-0.25;0.425,0.4;jeija_close_window.png;exit;]"..
-		"label[0.1,5;"..errmsg.."]")
+	meta:set_string("formspec", "size[12,10]"..
+		"background[-0.2,-0.25;12.4,10.75;jeija_luac_background.png]"..
+		"textarea[0.2,0.2;12.2,9.5;code;;"..code.."]"..
+		"image_button[4.75,8.75;2.5,1;jeija_luac_runbutton.png;program;]"..
+		"image_button_exit[11.72,-0.25;0.425,0.4;jeija_close_window.png;exit;]"..
+		"label[0.1,9;"..errmsg.."]")
 	meta:set_int("heat", 0)
 	meta:set_int("luac_id", math.random(1, 65535))
 end
