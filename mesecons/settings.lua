f70a308bb0bddefe963a6b82016f67d3be122069
AntumDeluge <antumdeluge@gmail.com>
2017-05-12 23:08:05 -0700

    Replace deprecated methods:
    - 'setting_get' with 'settings:get'
    - 'setting_getbool' with 'settings:get_bool'

--- mesecons/settings.lua
+++ mesecons/settings.lua
@@ -1,15 +1,15 @@
 -- SETTINGS
 function mesecon.setting(setting, default)
 	if type(default) == "boolean" then
-		local read = minetest.setting_getbool("mesecon."..setting)
+		local read = minetest.settings:get_bool("mesecon."..setting)
 		if read == nil then
 			return default
 		else
 			return read
 		end
 	elseif type(default) == "string" then
-		return minetest.setting_get("mesecon."..setting) or default
+		return minetest.settings:get("mesecon."..setting) or default
 	elseif type(default) == "number" then
-		return tonumber(minetest.setting_get("mesecon."..setting) or default)
+		return tonumber(minetest.settings:get("mesecon."..setting) or default)
 	end
 end
