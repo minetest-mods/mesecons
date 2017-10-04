3b8a9faf02229b4b5182adc6cd5eb007d84966f8
DS-Minetest <vorunbekannt75@web.de>
2017-10-04 17:26:58 +0200

    move rotate_rules functions to util.lua

--- mesecons/presets.lua
+++ mesecons/presets.lua
@@ -45,26 +45,23 @@ mesecon.rules.alldirs = {
 	{x =  0, y =  0,  z = -1},
 }
 
-local rules_wallmounted, rules_buttonlike = {}, {}
-minetest.after(0, function() -- The rotate_rules functions are not yet loaden.
-	rules_wallmounted = {
-		xp = mesecon.rotate_rules_down(mesecon.rules.floor),
-		xn = mesecon.rotate_rules_up(mesecon.rules.floor),
-		yp = mesecon.rotate_rules_up(mesecon.rotate_rules_up(mesecon.rules.floor)),
-		yn = mesecon.rules.floor,
-		zp = mesecon.rotate_rules_left(mesecon.rotate_rules_up(mesecon.rules.floor)),
-		zn = mesecon.rotate_rules_right(mesecon.rotate_rules_up(mesecon.rules.floor)),
-	}
+local rules_wallmounted = {
+	xp = mesecon.rotate_rules_down(mesecon.rules.floor),
+	xn = mesecon.rotate_rules_up(mesecon.rules.floor),
+	yp = mesecon.rotate_rules_up(mesecon.rotate_rules_up(mesecon.rules.floor)),
+	yn = mesecon.rules.floor,
+	zp = mesecon.rotate_rules_left(mesecon.rotate_rules_up(mesecon.rules.floor)),
+	zn = mesecon.rotate_rules_right(mesecon.rotate_rules_up(mesecon.rules.floor)),
+}
 
-	rules_buttonlike = {
-		xp = mesecon.rules.buttonlike,
-		xn = mesecon.rotate_rules_right(mesecon.rotate_rules_right(mesecon.rules.buttonlike)),
-		yp = mesecon.rotate_rules_down(mesecon.rules.buttonlike),
-		yn = mesecon.rotate_rules_up(mesecon.rules.buttonlike),
-		zp = mesecon.rotate_rules_right(mesecon.rules.buttonlike),
-		zn = mesecon.rotate_rules_left(mesecon.rules.buttonlike),
-	}
-end)
+local rules_buttonlike = {
+	xp = mesecon.rules.buttonlike,
+	xn = mesecon.rotate_rules_right(mesecon.rotate_rules_right(mesecon.rules.buttonlike)),
+	yp = mesecon.rotate_rules_down(mesecon.rules.buttonlike),
+	yn = mesecon.rotate_rules_up(mesecon.rules.buttonlike),
+	zp = mesecon.rotate_rules_right(mesecon.rules.buttonlike),
+	zn = mesecon.rotate_rules_left(mesecon.rules.buttonlike),
+}
 
 local function rules_from_dir(ruleset, dir)
 	if dir.x ==  1 then return ruleset.xp end
