1297a02836b03379baefbf43497d5c1b45d80a39
Oversword <bionic@oversword.co.uk>
2022-06-09 20:42:15 +0100

    Exit with error if vports do not exist for any reason

--- mesecons_luacontroller/init.lua
+++ mesecons_luacontroller/init.lua
@@ -471,6 +471,7 @@ local safe_globals = {
 local function create_environment(pos, mem, event, itbl, send_warning)
 	-- Gather variables for the environment
 	local vports = minetest.registered_nodes[minetest.get_node(pos).name].virtual_portstates
+	if not vports then return end
 	local vports_copy = {}
 	for k, v in pairs(vports) do vports_copy[k] = v end
 	local rports = get_real_port_states(pos)
@@ -627,6 +628,7 @@ local function run_inner(pos, code, event)
 	-- Create environment
 	local itbl = {}
 	local env = create_environment(pos, mem, event, itbl, send_warning)
+	if not env then return false, "Env does not exist. Controller has been moved?" end
 
 	local success, msg
 	-- Create the sandbox and execute code
