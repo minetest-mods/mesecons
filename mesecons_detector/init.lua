110b3c46b0a1fef2e9427b488d2b4bb25fd4e1a5
1F616EMO <root@1f616emo.xyz>
2024-07-02 05:55:32 +0800

    Simpilfy superfluous type checks

--- mesecons_detector/init.lua
+++ mesecons_detector/init.lua
@@ -223,10 +223,10 @@ local node_detector_digiline = {
 
 			if type(msg) == "table" then
 				if msg.distance or msg.scanname then
-					if msg.distance and type(msg.distance) == "string" then
+					if type(msg.distance) == "string" then
 						meta:set_string("distance", msg.distance)
 					end
-					if msg.scanname and type(msg.scanname) == "string" then
+					if type(msg.scanname) == "string" then
 						meta:set_string("scanname", msg.scanname)
 					end
 					node_detector_make_formspec(pos)
