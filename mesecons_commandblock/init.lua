0120aac472baed9ca41d0c9e6fd1551467f6f6f4
tour <129965577+a-tour-ist@users.noreply.github.com>
2024-11-14 21:11:01 +0100

    commandblock: replace deprecated formspec element "invsize"

--- mesecons_commandblock/init.lua
+++ mesecons_commandblock/init.lua
@@ -48,7 +48,7 @@ minetest.register_chatcommand("hp", {
 local function initialize_data(meta)
 	local commands = minetest.formspec_escape(meta:get_string("commands"))
 	meta:set_string("formspec",
-		"invsize[9,5;]" ..
+		"size[9,5]" ..
 		"textarea[0.5,0.5;8.5,4;commands;Commands;"..commands.."]" ..
 		"label[1,3.8;@nearest, @farthest, and @random are replaced by the respective player names]" ..
 		"button_exit[3.3,4.5;2,1;submit;Submit]")
