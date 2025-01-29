55359c7a0bded79913542369418d384ff7f5bcc3
blut <stuff@waifu.club>
2025-01-29 13:34:27 +0100

    mesecons_extrawires: Fix Mineclonia compatibility

--- mesecons_extrawires/init.lua
+++ mesecons_extrawires/init.lua
@@ -3,4 +3,6 @@ dofile(minetest.get_modpath("mesecons_extrawires").."/tjunction.lua");
 dofile(minetest.get_modpath("mesecons_extrawires").."/corner.lua");
 dofile(minetest.get_modpath("mesecons_extrawires").."/doublecorner.lua");
 dofile(minetest.get_modpath("mesecons_extrawires").."/vertical.lua");
-dofile(minetest.get_modpath("mesecons_extrawires").."/mesewire.lua");
+if core.registered_nodes["mesecons_gamecompat:mese"] then
+	dofile(minetest.get_modpath("mesecons_extrawires").."/mesewire.lua");
+end
