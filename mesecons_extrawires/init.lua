d5835a230e7d60a937507c57f58c8d7160d2059f
blut <stuff@waifu.club>
2025-02-01 17:49:25 +0100

    fix regression from fixing mineclonia crash in mesecons extrawires, mesewire wouldn't be registered in mtg

--- mesecons_extrawires/init.lua
+++ mesecons_extrawires/init.lua
@@ -3,6 +3,6 @@ dofile(minetest.get_modpath("mesecons_extrawires").."/tjunction.lua");
 dofile(minetest.get_modpath("mesecons_extrawires").."/corner.lua");
 dofile(minetest.get_modpath("mesecons_extrawires").."/doublecorner.lua");
 dofile(minetest.get_modpath("mesecons_extrawires").."/vertical.lua");
-if core.registered_nodes[mese_nodename] then
+if core.registered_nodes["mesecons_gamecompat:mese"] then
 	dofile(minetest.get_modpath("mesecons_extrawires").."/mesewire.lua");
 end
